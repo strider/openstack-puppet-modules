d0a7ee26e862d3529f054a9b44e3e7bed33af30e
Martin Mágr <mmagr@redhat.com>
2014-11-13 17:21:52 +0100

    [glance] Replace qpid_host with qpid_hostname
    This patch replaces `qpid_host` with `qpid_hostname` in both the
    qpid manifest and the spec file class. `qpid_host` has been shipped
    as the parameter to specify qpid's hostname, although the correct one is
    `qpid_hostname`.
    
    Closes-bug 1242715
    
    Change-Id: I92b8da5a204180316f8117634a45154023551abd
    
    Conflicts:
    
        glance/manifests/notify/qpid.pp

--- glance/manifests/notify/qpid.pp
+++ glance/manifests/notify/qpid.pp
@@ -13,6 +13,7 @@ class glance::notify::qpid(
   }
 
   glance_api_config {
+    'DEFAULT/notifier_strategy': value => 'qpid';
     'DEFAULT/qpid_hostname':     value => $qpid_hostname;
     'DEFAULT/qpid_port':         value => $qpid_port;
     'DEFAULT/qpid_username':     value => $qpid_username;
