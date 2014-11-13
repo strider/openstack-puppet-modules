0f1e66d16ef698117eaf87a230bfd3dfbfdd6752
Martin Mágr <mmagr@redhat.com>
2014-11-13 17:23:08 +0100

    [glance] Add parameter qpid_protocol to notify::qpid so ssl can be enabled.
    Bug: #1251050
    Change-Id: Ibc3b5e19b2543ff33366f305e7b9cef61dddf2f1
    
    Conflicts:
    
        glance/manifests/notify/qpid.pp

--- glance/manifests/notify/qpid.pp
+++ glance/manifests/notify/qpid.pp
@@ -6,6 +6,7 @@ class glance::notify::qpid(
   $qpid_username = 'guest',
   $qpid_hostname = 'localhost',
   $qpid_port     = '5672',
+  $qpid_protocol = 'tcp'
 ) inherits glance::api {
 
   Glance_api_config <| title == 'DEFAULT/notifier_strategy' |> {
@@ -16,6 +17,7 @@ class glance::notify::qpid(
     'DEFAULT/notifier_strategy': value => 'qpid';
     'DEFAULT/qpid_hostname':     value => $qpid_hostname;
     'DEFAULT/qpid_port':         value => $qpid_port;
+    'DEFAULT/qpid_protocol':     value => $qpid_protocol;
     'DEFAULT/qpid_username':     value => $qpid_username;
     'DEFAULT/qpid_password':     value => $qpid_password;
   }
