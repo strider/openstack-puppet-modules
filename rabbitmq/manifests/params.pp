2180b43f35c506fa6bce7533a3bc82f77bce5b3b
Lukas Bezdicka <lbezdick@redhat.com>
2015-06-17 14:38:23 +0200

    [Rabbitmq] set repos_ensure to false
    We should use rabbitmq only from repos provided to us by RDO, RHOS or distro.
    
    Change-Id: I53ea9c10800846d5d0e6632b57e969159d64ff90

--- rabbitmq/manifests/params.pp
+++ rabbitmq/manifests/params.pp
@@ -68,7 +68,7 @@ class rabbitmq::params {
   $management_port            = '15672'
   $package_apt_pin            = ''
   $package_gpg_key            = 'http://www.rabbitmq.com/rabbitmq-signing-key-public.asc'
-  $repos_ensure               = true
+  $repos_ensure               = false
   $manage_repos               = undef
   $service_ensure             = 'running'
   $service_manage             = true
