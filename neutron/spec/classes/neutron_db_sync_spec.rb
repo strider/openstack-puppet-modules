1cdb9d7650019b253297f1b002c869b18895d264
Lukas Bezdicka <lbezdick@redhat.com>
2015-07-12 05:49:00 -0400

    Run neutron db sync also for each neutron module
    FWaaS, VPNaaS and LBaaS packages need to be installed on neutron API
     node otherwise the neutron service won't start.
    
    Also if these neutron modules are enabled one has to update DB for these
    modules in neutron::db::sync.
    
    Change-Id: I9b2c00309f3a72f76fffd22a6f3b802e5f78dad2
    Closes-Bug: rhbz#1233149, rhbz#1230199, #1474961

--- neutron/spec/classes/neutron_db_sync_spec.rb
+++ neutron/spec/classes/neutron_db_sync_spec.rb
@@ -40,4 +40,45 @@ describe 'neutron::db::sync' do
     it_configures 'neutron-dbsync'
   end
 
+  context 'with neutron::service_plugins' do
+    let :facts do
+      {
+        :osfamily                 => 'RedHat',
+        :operatingsystemrelease   => '7.0',
+        :concat_basedir => '/var/lib/puppet/concat'
+      }
+    end
+
+    let :params do
+      {
+        :service_plugins => ['router','firewall','lbaas','vpnaas','metering']
+      }
+    end
+
+    it 'runs neutron-db-sync-lbaas' do
+      is_expected.to contain_exec('neutron-db-sync-lbaas').with(
+        :command     => 'neutron-db-manage  --service lbaas --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
+        :path        => '/usr/bin',
+        :refreshonly => 'true',
+        :logoutput   => 'on_failure'
+      )
+    end
+    it 'runs neutron-db-sync-vpnaas' do
+      is_expected.to contain_exec('neutron-db-sync-vpnaas').with(
+        :command     => 'neutron-db-manage  --service vpnaas --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
+        :path        => '/usr/bin',
+        :refreshonly => 'true',
+        :logoutput   => 'on_failure'
+      )
+    end
+    it 'runs neutron-db-sync-fwaas' do
+      is_expected.to contain_exec('neutron-db-sync-fwaas').with(
+        :command     => 'neutron-db-manage  --service fwaas --config-file /etc/neutron/neutron.conf --config-file /etc/neutron/plugin.ini upgrade head',
+        :path        => '/usr/bin',
+        :refreshonly => 'true',
+        :logoutput   => 'on_failure'
+      )
+    end
+  end
+
 end
